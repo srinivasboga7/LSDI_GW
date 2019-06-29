267b3f26864fb865f157e92b6de15e8de7340341
sumanth <sumanthcherupally@gmail.com>
2019-06-29 21:18:56 +0530

    Added discovery protocol(temporary) and removed from client

--- client/client.go
+++ client/client.go
@@ -2,9 +2,6 @@ package client
 
 import (
 	"math/rand"
-	"io/ioutil"
-	"strings"
-	"net"
 	dt "GO-DAG/DataTypes"
 	"time"
 	"encoding/binary"
@@ -43,29 +40,6 @@ func BroadcastTransaction(b []byte, p dt.Peers) {
 	p.Mux.Unlock()
 }
 
-func GetIps(filename string) []string {
-	//Returns slice which has IPs as strings
-	//Currently getting from file
-	//Later get from DNS server or anything else
-	bytes, err := ioutil.ReadFile(filename)
-	if err != nil {
-		log.Fatal(err)
-	}
-	dataslice := strings.Split(string(bytes),"\n")
-	dataslice = dataslice[:len(dataslice)-1]         //Delete last entry from slice as it will be empty
-	return dataslice
-}
-
-func ConnectToServer(ips []string)(dt.Peers) {
-	//Takes a slice of ips and returns sock file descriptors of tcp connections
-	var p dt.Peers
-	for _,ip := range ips {
-		tcpAddr, _ := net.ResolveTCPAddr("tcp4", ip)
-		conn, _ := net.DialTCP("tcp", nil, tcpAddr)    //BLocking call
-		p.Fds = append(p.Fds,conn)
-	}
-	return p
-}
 
 func SimulateClient(p dt.Peers) {
 
