5234b5e530f99be6ecf6acee344d6665d8b8e347
srinivasboga7 <cs17btech11009@iith.ac.in>
2019-07-03 18:54:54 +0530

    fixed serializing signature

--- Crypto/crypto.go
+++ Crypto/crypto.go
@@ -7,11 +7,13 @@ import (
 	"crypto/x509"
 	"crypto/elliptic"
 	"encoding/pem"
+	"math/big"
 	"os"
-	"ioutil"
+	"io/ioutil"
+	"fmt"
 )
 
-func hash(b []byte) [32]byte {
+func Hash(b []byte) [32]byte {
 	h := sha256.Sum256(b)
 	return h
 }
@@ -35,13 +37,15 @@ func SerializePublicKey(PublicKey *ecdsa.PublicKey) []byte {
 }
 
 func SerializePrivateKey(privateKey *ecdsa.PrivateKey) []byte{
-	key,_ := MarshalECPrivateKey(privateKey)
+	key,_ := x509.MarshalECPrivateKey(privateKey)
 	return key
 }
 
+
 func DeserializePublicKey(data []byte) *ecdsa.PublicKey {
 	PublicKey,_ := x509.ParsePKIXPublicKey(data)
-	return PublicKey
+	PubKey,_ := PublicKey.(*ecdsa.PublicKey)
+	return PubKey
 }
 
 func DeserializePrivateKey(data []byte) *ecdsa.PrivateKey {
@@ -49,13 +53,6 @@ func DeserializePrivateKey(data []byte) *ecdsa.PrivateKey {
 	return PrivateKey
 }
 
-func SerializeSignature(r *big.Int,s *big.Int) []byte{
-
-}
-
-func DeserializeSignature(sig []byte) *big.Int {
-
-}
 
 func GenerateKeys() *ecdsa.PrivateKey {
 	// function generates keys and creates a pem file with key stored in that
@@ -63,7 +60,7 @@ func GenerateKeys() *ecdsa.PrivateKey {
 	if err != nil {
 		fmt.Println(err)
 	}
-	serialKey := SerializePrivateKey(&PrivateKey)
+	serialKey := SerializePrivateKey(PrivateKey)
 	file,_ := os.OpenFile("PrivateKey.pem", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
 	pemBlock := &pem.Block{Type: "EC PRIVATE KEY", Bytes: serialKey} // serializing and writing to the pem file
 	if err := pem.Encode(file,pemBlock) ; err != nil {
@@ -80,19 +77,34 @@ func LoadKeys() *ecdsa.PrivateKey {
 	if err != nil {
 		fmt.Println(err)
 	}
-	block,rest := pem.Decode(bytes)
+	block,_ := pem.Decode(bytes)
 	PrivateKey := DeserializePrivateKey(block.Bytes)
 	return PrivateKey
 }
 
 func Sign(hash []byte, key *ecdsa.PrivateKey) []byte {
 	// returns the serialized form of the signature
-	// figure out how to serialize signature
 	var signature []byte
 	if len(hash) != 32 { // check the length of hash
 		fmt.Println("Invalid hash")
 		return signature
 	}
-	r,s,err := ecdsa.Sign(rand.Reader,key,hash)
+	r,s,_ := ecdsa.Sign(rand.Reader,key,hash)
 
+	signature = r.Bytes()
+	signature = append(signature,s.Bytes()...)
+	return signature
+}
+
+func Verify(signature []byte , PublicKey *ecdsa.PublicKey, hash []byte) bool {
+	if len(signature) != 64 {
+		fmt.Println("Invalid Signature")
+		return false
+	}
+	r := new(big.Int)
+	s := new(big.Int)
+	r.SetBytes(signature[:32])
+	s.SetBytes(signature[32:])
+
+	return ecdsa.Verify(PublicKey,hash,r,s)
 }
