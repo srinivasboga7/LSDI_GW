b908a4b89936b24a13395049987f03fbb0a20a04
srinivasboga7 <cs17btech11009@iith.ac.in>
2019-07-19 19:48:07 +0530

    fixed Duplication issue

--- storage/Dag.go
+++ storage/Dag.go
@@ -7,14 +7,33 @@ import(
 	"GO-DAG/Crypto"
 )
 
-func AddTransaction(dag dt.DAG,c chan dt.Transaction) {
-	for {
-		var node dt.Node
-		node.Tx = <-c
-		s := serialize.SerializeData(node.Tx)
-		hash := Crypto.Hash(s)
-		h := Crypto.EncodeToHex(hash[:])
-		dag.Graph[h] = node
-		fmt.Println("Succesfully added")
+func AddTransaction(dag dt.DAG,tx dt.Transaction) bool {
+	
+	var node dt.Node
+	var duplicationCheck bool
+	duplicationCheck = false
+	s := serialize.SerializeData(tx)
+	hash := Crypto.Hash(s)
+	h := Crypto.EncodeToHex(hash[:])
+	dag.Mux.Lock()
+	if _,ok := dag.Graph[h] ; !ok{  // Duplication check
+		node.Tx = tx
+		dag.Graph[h] = node  
+		l := dag.Graph[Crypto.EncodeToHex(tx.LeftTip[:])]
+		r := dag.Graph[Crypto.EncodeToHex(tx.RightTip[:])]
+		if l.Tx == r.Tx {
+			l.Neighbours = append(l.Neighbours,h)
+			dag.Graph[Crypto.EncodeToHex(tx.LeftTip[:])] = l
+		} else {
+			l.Neighbours = append(l.Neighbours,h)
+			dag.Graph[Crypto.EncodeToHex(tx.LeftTip[:])] = l
+			r.Neighbours = append(r.Neighbours,h)
+			dag.Graph[Crypto.EncodeToHex(tx.RightTip[:])] = r
+		}
+		//fmt.Println("Succesfully added")
+		duplicationCheck = true
+		fmt.Println("Added Transaction", h)
 	}
+	dag.Mux.Unlock()
+	return duplicationCheck
 }
