0b8676e864f13336028547caeefee5d044e7e77d
srinivasboga7 <cs17btech11009@iith.ac.in>
2019-07-19 19:47:34 +0530

    fixed Duplication issue

--- client/client.go
+++ client/client.go
@@ -6,7 +6,10 @@ import (
 	"time"
 	"GO-DAG/Crypto"
 	"GO-DAG/serialize"
+	"GO-DAG/consensus"
+	"GO-DAG/storage"
 	"crypto/ecdsa"
+	//"fmt"
 )
 
 
@@ -15,8 +18,11 @@ func GenerateMessage(b []byte, PrivateKey *ecdsa.PrivateKey) []byte {
 	signature := Crypto.Sign(hash[:],PrivateKey)
 	var l uint32
 	l = uint32(len(b))
+	var magic_number uint32
+	magic_number = 1
 	payload := append(b,signature...)
 	payload = append(serialize.EncodeToBytes(l),payload...)
+	payload = append(serialize.EncodeToBytes(magic_number),payload...)
 	return payload
 }
 
@@ -29,8 +35,7 @@ func BroadcastTransaction(b []byte, p dt.Peers) {
 	p.Mux.Unlock()
 }
 
-
-func SimulateClient(p dt.Peers, PrivateKey *ecdsa.PrivateKey) {
+func SimulateClient(p dt.Peers, PrivateKey *ecdsa.PrivateKey, dag dt.DAG) {
 
 	var tx dt.Transaction
 
@@ -38,9 +43,15 @@ func SimulateClient(p dt.Peers, PrivateKey *ecdsa.PrivateKey) {
 		tx.Timestamp = time.Now().Unix()
 		tx.Value = rand.Float64()
 		copy(tx.From[:],Crypto.SerializePublicKey(&PrivateKey.PublicKey))
+		copyLedger := dag
+		copy(tx.LeftTip[:],Crypto.DecodeToBytes(consensus.GetTip(copyLedger,0.1)))
+		copy(tx.RightTip[:],Crypto.DecodeToBytes(consensus.GetTip(copyLedger,0.1)))
+		Crypto.PoW(&tx,4)
+		//fmt.Println("Selected Tips")
+		storage.AddTransaction(dag,tx)
 		buffer := serialize.SerializeData(tx)
 		msg := GenerateMessage(buffer,PrivateKey)
 		BroadcastTransaction(msg,p)
-		time.Sleep(2*time.Second)
+		time.Sleep(time.Second)
 	}
 }
