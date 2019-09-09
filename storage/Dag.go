dc8c6380710dd53432435b28653f536093e5395b
srinivasboga7 <cs17btech11009@iith.ac.in>
2019-09-09 15:15:29 +0530

    fixed orphan transactions

--- storage/Dag.go
+++ storage/Dag.go
@@ -5,42 +5,73 @@ import(
 	"fmt"
 	"GO-DAG/serialize"
 	"GO-DAG/Crypto"
-	//"GO-DAG/consensus"
+	"sync"
 )
+
+var OrphanedTransactions = make(map[string]dt.Node)
+var Mux sync.Mutex 
+
 
 func AddTransaction(dag *dt.DAG,tx dt.Transaction, signature []byte) bool {
 
+	// change this function for the storage node
 	var node dt.Node
 	var duplicationCheck bool
 	duplicationCheck = false
 	s := serialize.SerializeData(tx)
 	hash := Crypto.Hash(s)
 	h := Crypto.EncodeToHex(hash[:])
+
 	dag.Mux.Lock()
-	if _,ok := dag.Graph[h] ; !ok{  // Duplication check
+	if _,ok := dag.Graph[h] ; !ok{  //Duplication check
 		node.Tx = tx
 		node.Signature = signature
-		dag.Graph[h] = node  
-		l,ok := dag.Graph[Crypto.EncodeToHex(tx.LeftTip[:])]
-		if !ok {
-			fmt.Println("something wrong")
-		}
-		r,ok := dag.Graph[Crypto.EncodeToHex(tx.RightTip[:])]
-		if !ok {
-			fmt.Println("something wrong")
-		}
-		if l.Tx == r.Tx {
-			l.Neighbours = append(l.Neighbours,h)
-			dag.Graph[Crypto.EncodeToHex(tx.LeftTip[:])] = l
+		left := Crypto.EncodeToHex(tx.LeftTip[:])
+		right := Crypto.EncodeToHex(tx.RightTip[:]) 
+		l,ok_l := dag.Graph[left]
+		r,ok_r := dag.Graph[right]
+		if !ok_l || !ok_r {
+			if !ok_l {
+				OrphanedTransactions[left] = node
+				fmt.Println("Orphaned Transactions")	
+			}
+			if !ok_r {
+				fmt.Println("Orphaned Transactions")
+				OrphanedTransactions[right] = node
+			}
 		} else {
-			l.Neighbours = append(l.Neighbours,h)
-			dag.Graph[Crypto.EncodeToHex(tx.LeftTip[:])] = l
-			r.Neighbours = append(r.Neighbours,h)
-			dag.Graph[Crypto.EncodeToHex(tx.RightTip[:])] = r
+			dag.Graph[h] = node
+			if left == right {
+				l.Neighbours = append(l.Neighbours,h)
+				dag.Graph[Crypto.EncodeToHex(tx.LeftTip[:])] = l
+			} else {
+				l.Neighbours = append(l.Neighbours,h)
+				dag.Graph[Crypto.EncodeToHex(tx.LeftTip[:])] = l
+				r.Neighbours = append(r.Neighbours,h)
+				dag.Graph[Crypto.EncodeToHex(tx.RightTip[:])] = r
+			}
+			duplicationCheck = true
+			//fmt.Println("Added Transaction ",h)
 		}
-		duplicationCheck = true
-		//fmt.Println("Added Transaction", h)
 	}
 	dag.Mux.Unlock()
+	if duplicationCheck {
+		checkOrphanedTransactions(h,dag)
+	}
 	return duplicationCheck
+}
+
+func checkOrphanedTransactions(h string,dag *dt.DAG) {
+	Mux.Lock()
+	node,ok := OrphanedTransactions[h]
+	Mux.Unlock()
+	if ok {
+		if AddTransaction(dag,node.Tx,node.Signature) {
+			//fmt.Println("resolved Transaction")
+		}
+	}
+	Mux.Lock()
+	delete(OrphanedTransactions,h)
+	Mux.Unlock()
+	return 
 }
