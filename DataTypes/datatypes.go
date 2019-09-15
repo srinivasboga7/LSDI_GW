27f7607f0e623607fbebecc93b12aed25b6af7c1
srinivasboga7 <cs17btech11009@iith.ac.in>
2019-09-15 14:34:28 +0530

    changed Tx structure

--- DataTypes/datatypes.go
+++ DataTypes/datatypes.go
@@ -6,44 +6,33 @@ import(
 	"net"
 )
 
-
+// Transaction defines the structure of the transaction in the blockchain
 type Transaction struct {
-	// Definition of data
-	Timestamp int64
-	Value float64 //could be a string but have to figure out serialization
+	Timestamp int64 // 8 bytes
+	Hash [32]byte //could be a string but have to figure out serialization
 	From [65]byte //length of public key 33(compressed) or 65(uncompressed)
-	//Ntips uint16
-	//Tips [Ntips][32]byte
+	TxID [16]byte
 	LeftTip [32]byte
 	RightTip [32]byte
-	Nonce uint32 //temporary based on type of PoW
+	Nonce uint32 // 4 bytes
 }
 
+// Peers maintains the list of all peers connected to the node
 type Peers struct {
 	Mux sync.Mutex
 	Fds map[string] net.Conn
 }
 
-type Node struct {
+// Vertex is a wrapper struct of Transaction 
+type Vertex struct {
 	Tx Transaction
 	Signature []byte
-	Neighbours [] string // pointers to the neighbours which gets updated when this node is chosen as tip
-
+	Neighbours [] string 
 }
 
+// DAG defines the data structure to store the blockchain
 type DAG struct {
 	Mux sync.Mutex
-	/*
-	ChTx chan dt.Node
-	ChDuplicateCheck chan string
-	ChTipSelection chan string
-	*/
 	Genisis string
-	Graph map[string] Node // string is the hash of the transaction(Node.Tx)
-}
-
-type Request struct {
-	Data []byte
-	IP string
-	Conn net.Conn
+	Graph map[string] Vertex
 }
--- client/client.go
+++ client/client.go
@@ -1,30 +1,48 @@
 package client
 
 import (
+	"encoding/json"
 	"math/rand"
 	dt "GO-DAG/DataTypes"
 	"time"
+	"net/http"
+	"github.com/google/uuid"
 	"GO-DAG/Crypto"
 	"GO-DAG/serialize"
 	"GO-DAG/consensus"
 	"GO-DAG/storage"
 	"crypto/ecdsa"
 	"fmt"
+	"bytes"
 )
 
+type sensordata struct {
+	sensorName string
+	sensorID [32]byte
+	value float64
+	startTime int64
+	endTime int64
+}
 
+type postRequest struct {
+	data sensordata
+	ID [16]byte
+}
+
+// GenerateMessage creates a byte slice to be sent over a network
+// for a particular transaction.
 func GenerateMessage(b []byte, signature []byte) []byte {
 	var l uint32
 	l = uint32(len(b))
-	var magic_number uint32
-	magic_number = 1
+	var magicNumber uint32
+	magicNumber = 1
 	payload := append(b,signature...)
 	payload = append(serialize.EncodeToBytes(l),payload...)
-	payload = append(serialize.EncodeToBytes(magic_number),payload...)
+	payload = append(serialize.EncodeToBytes(magicNumber),payload...)
 	return payload
 }
 
-
+// BroadcastTransaction sends the transaction to all the peers
 func BroadcastTransaction(b []byte, p *dt.Peers) {
 	p.Mux.Lock()
 	for _,conn := range p.Fds {
@@ -33,15 +51,38 @@ func BroadcastTransaction(b []byte, p *dt.Peers) {
 	p.Mux.Unlock()
 }
 
+// GenerateSignature uses Crypto pkg to genearte a signature.
+// It is a wrapper aroung Crypto.Sign function
 func GenerateSignature(b []byte, PrivateKey *ecdsa.PrivateKey) []byte {
 	hash := Crypto.Hash(b)
 	signature := Crypto.Sign(hash[:],PrivateKey)
 	return signature
 }
 
-func SimulateClient(p *dt.Peers, PrivateKey *ecdsa.PrivateKey, dag *dt.DAG) {
+// Copy generates the Deep copy of the DAG
+func Copy(dag *dt.DAG) *dt.DAG {
+	bytes,_ := json.Marshal(dag.Graph)
+	var copyGraph map[string] dt.Vertex
+	json.Unmarshal(bytes,&copyGraph)
+	var copyDag dt.DAG
+	copyDag.Graph = copyGraph
+	copyDag.Genisis = dag.Genisis
+	return &copyDag
+}
+
+func fakeSensorData(data *sensordata) {
+	data.value = rand.Float64()
+	data.startTime = time.Now().Unix() - 5
+	data.endTime = time.Now().Unix() 
+}
+
+// SimulateClient is used for testing by sending fake data as transactions.
+func SimulateClient(p *dt.Peers, PrivateKey *ecdsa.PrivateKey, dag *dt.DAG, url string) {
 
 	var tx dt.Transaction
+	var data sensordata
+	data.sensorName = "PowerReading"
+	data.sensorID = Crypto.Hash(Crypto.SerializePublicKey(&PrivateKey.PublicKey))
 
 	for {
 		p.Mux.Lock()
@@ -56,19 +97,28 @@ func SimulateClient(p *dt.Peers, PrivateKey *ecdsa.PrivateKey, dag *dt.DAG) {
 	fmt.Println("Generating Transactions")
 
 	for {
+		fakeSensorData(&data)
 		tx.Timestamp = time.Now().Unix()
-		tx.Value = rand.Float64()
+		serial,_ := json.Marshal(data)
+		tx.Hash = Crypto.Hash(serial)
+		tx.TxID = uuid.New()
+		var request postRequest 
+		request.data = data
+		request.ID = tx.TxID
+		b := bytes.NewReader(serial)
+		http.Post(url,"application/json",b)
 		copy(tx.From[:],Crypto.SerializePublicKey(&PrivateKey.PublicKey))
 		dag.Mux.Lock()
-		copy(tx.LeftTip[:],Crypto.DecodeToBytes(consensus.GetTip(dag,0.01)))
-		copy(tx.RightTip[:],Crypto.DecodeToBytes(consensus.GetTip(dag,0.01)))
+		copyDag := Copy(dag)
 		dag.Mux.Unlock()
+		copy(tx.LeftTip[:],Crypto.DecodeToBytes(consensus.GetTip(copyDag,0.01)))
+		copy(tx.RightTip[:],Crypto.DecodeToBytes(consensus.GetTip(copyDag,0.01)))
 		Crypto.PoW(&tx,2)
 		buffer := serialize.SerializeData(tx)
 		sign := GenerateSignature(buffer,PrivateKey)
 		msg := GenerateMessage(buffer,sign)
 		storage.AddTransaction(dag,tx,sign)
 		BroadcastTransaction(msg,p)
-		time.Sleep(time.Second)
+		time.Sleep(5*time.Second)
 	}
 }
